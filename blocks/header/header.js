5d81bd01928d12f67ff8d418dfaf8155458b43cc
acn-ariel <ariel.s.udtohan@accenture.com>
2026-07-31 18:46:24 +0800

    add en languange

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -200,7 +200,7 @@ async function buildBreadcrumbs() {
  */
 export default async function decorate(block) {
   // Language root detection and path adjustment
-  const supportedLocales = ['es', 'fr'];
+  const supportedLocales = ['en', 'es', 'fr'];
   const pathParts = window.location.pathname.split('/').filter(Boolean);
   let langRoot = '';
   if (pathParts.length > 0 && supportedLocales.includes(pathParts[0])) {
